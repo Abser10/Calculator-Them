51fde8b4eb0c437499c097f28a46975dd85d76d7
Ahmedabser <ahmedabser6060@gmail.com>
2025-03-25 10:59:01 +0200

    .

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
+let resultDisplayed = false;
 
 buttons.forEach((button) => {
     button.onclick = () => {
         if (button.id == 'clear') {
             display.innerText = '';
+            resultDisplayed = false; // Reset flag
         } else if (button.id == 'backspace') {
             let string = display.innerText.toString();
             display.innerText = string.substr(0, string.length - 1);
         } else if (display.innerText !== '' && button.id == 'equal') {
             try {
                 display.innerText = eval(display.innerText);
+                resultDisplayed = true;
             } catch {
                 display.innerText = 'Error';
                 setTimeout(() => display.innerText = '', 2000);
+                resultDisplayed = false;
             }
         } else if (display.innerText === '' && button.id == 'equal') {
             display.innerText = 'Empty!';
             setTimeout(() => display.innerText = '', 2000);
+            resultDisplayed = false;
         } else {
+            if (resultDisplayed) {
+                display.innerText = '';
+                resultDisplayed = false;
+            }
             display.innerText += button.id;
         }
     };
